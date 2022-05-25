c105c4d21685da24dc4d8f2e24a62651a02ffe11
Lucas Andrade <89672948+LucaasAndrade@users.noreply.github.com>
2022-05-25 16:03:44 -0300

    Update index.js

--- src/pages/maiorNumero/index.js
+++ src/pages/maiorNumero/index.js
@@ -1,13 +1,26 @@
-
-
-
+import axios from "axios"
+import { useState } from "react"
 
 
 export default function Index(){
+    const [numero, setNumero] = useState([]);
+    const [resposta, setResposta] = useState(0);
+
+    async function maiorNumero(){
+        const resp = await axios('http://localhost:5000/dia2/maiorNumero', {
+            a: numero
+        })
+        setResposta(resp.data.a)
+    }
+
     return (
+
         <main>
             <h1> Maior Número </h1>
-            <input></input>
+            <input type='text' value={numero} onChange={e => setNumero(e.target.value)} />
+    
+            <button onClick={maiorNumero}/>
+            <p> O maior núemero é {resposta}</p>
         </main>
     )
 }
