61b200bd105b953c9ba13721837c4abafe9422da
Lucas Andrade <89672948+LucaasAndrade@users.noreply.github.com>
2022-05-25 15:21:53 -0300

    Update index.js

--- src/pages/frequencia/index.js
+++ src/pages/frequencia/index.js
@@ -3,21 +3,25 @@ import { useState } from "react"
 
 
 export default function Index(){
-    const [numero, setNumero] = useState(0);
+    const [texto, setTexto] = useState('');
+    const [caracter, setCaracter] = useState('');
     const [resp, setResposta] = useState(0);
 
-    async function CalcularDobro(){
-        const resposta = await axios.get('http://localhost:5000/dobro/' + numero);
-        setResposta(resposta.data.dobro)
+    async function VerFrequencia() {
+        const resposta = await axios.get('http://localhost:5000/dia2/freqCaracter/' + texto + '/' + caracter);
+        setResposta(resposta.data.freq);
     }
     return (
         <main>
-            <h1> Calcular Dobro </h1>
-            numero: <input type='text' value={numero} onChange={e => setNumero(e.target.value)} />
+            <h1> Frequência </h1>
+            <p> Digite uma palavra </p>
+            texto: <input type='text' value={texto} onChange={e => setTexto(e.target.value)} />
+            <p>Digite a letra para ver a Frequência</p>
+            caracter: <input type='text' value={caracter} onChange={e => setCaracter(e.target.value)} />
 
-            <button onClick={CalcularDobro}> Calcular </button>
+            <button onClick={VerFrequencia} > Checar</button>
 
-            <p> O dobro de {numero} é {resp}</p>
+            <p> O caracter "{caracter}" aparece {resp} vezes no texto. </p>
         </main>
     )
 }
