54a8680abb205a665444819a53aa0c317f149fd5
Lucas Andrade <89672948+LucaasAndrade@users.noreply.github.com>
2022-05-25 15:20:26 -0300

    Update index.js

--- src/pages/dobro/index.js
+++ src/pages/dobro/index.js
@@ -1,13 +1,23 @@
-
-
-
+import axios from "axios"
+import { useState } from "react"
 
 
 export default function Index(){
+    const [numero, setNumero] = useState(0);
+    const [resp, setResposta] = useState(0);
+
+    async function CalcularDobro(){
+        const resposta = await axios.get('http://localhost:5000/dobro/' + numero);
+        setResposta(resposta.data.dobro)
+    }
     return (
         <main>
             <h1> Calcular Dobro </h1>
-            <input></input>
+            numero: <input type='text' value={numero} onChange={e => setNumero(e.target.value)} />
+
+            <button onClick={CalcularDobro}> Calcular </button>
+
+            <p> O dobro de {numero} é {resp}</p>
         </main>
     )
 }
